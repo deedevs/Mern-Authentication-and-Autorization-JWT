c1db6498a1db87bfddb231163b07937f546a9feb
David Ladipo <64803207+deedevs@users.noreply.github.com>
2021-01-05 22:56:44 +0200

    implemented Register, login and logout routes

--- routes/User.js
+++ routes/User.js
@@ -1,10 +1,21 @@
-const express = require(express);
+const express = require("express");
 const userRouter = express.Router();
 const passport = require("passport");
 const passportConfig = require("../passport");
 const JWT = require("jsonwebtoken");
 const User = require("../models/User");
 const Todo = require("../models/Todo");
+
+const signToken = (userID) => {
+  return JWT.sign(
+    {
+      iss: "DeeDevs",
+      sub: userID,
+    },
+    "DeeDevs",
+    { expiresIn: "1h" }
+  );
+};
 
 userRouter.post("/register", (req, res) => {
   const { username, password, role } = req.body;
@@ -13,5 +24,53 @@ userRouter.post("/register", (req, res) => {
       res
         .status(500)
         .json({ message: { msgBody: "Error has occured", msgError: true } });
+    if (user)
+      res.status(400).json({
+        message: { msgBody: "User name is already Taken", msgError: true },
+      });
+    else {
+      const newUser = new User({
+        username,
+        password,
+        role,
+      });
+      newUser.save((err) => {
+        if (err)
+          res.status(500).json({
+            message: { msgBody: "Error has occured", msgError: true },
+          });
+        else
+          res.status(201).json({
+            message: {
+              msgBody: "Account sucessfully created",
+              msgError: false,
+            },
+          });
+      });
+    }
   });
 });
+
+userRouter.post(
+  "/login",
+  passport.authenticate("local", { session: false }),
+  (req, res) => {
+    if (req.isAuthenticated()) {
+      const { _id, username, role } = req.user;
+      const token = signToken(_id);
+      res.cookie("access_token", token, { httpOnly: true, sameSite: true });
+      res.status(200).json({ isAuthenticated: true, user: { username, role } });
+    }
+  }
+);
+
+userRouter.get(
+  "/logout",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.clearCookie("access_token");
+    res.json({ user: { username: "", role: "" }, success: true });
+  }
+);
+
+module.exports = userRouter;
