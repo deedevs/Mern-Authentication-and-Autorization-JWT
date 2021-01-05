30861d0bfee83ecde6492289ba60f38d3b7099a5
David Ladipo <64803207+deedevs@users.noreply.github.com>
2021-01-05 11:40:49 +0200

    set up server and connected to mongodb database

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const connectDB = require("./db");
+const dotenv = require("dotenv");
+
+app.use(cookieParser());
+app.use(express.json());
+
+dotenv.config();
+
+connectDB();
+
+app.listen(5000, () => {
+  console.log("express server started");
+});
