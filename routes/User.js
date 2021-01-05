e2dece96111586386d9ae767397307b26fb3ccf6
David Ladipo <64803207+deedevs@users.noreply.github.com>
2021-01-05 23:40:21 +0200

    implemented todo, admin, adn authenticated routes

--- routes/User.js
+++ routes/User.js
@@ -73,4 +73,75 @@ userRouter.get(
   }
 );
 
+userRouter.post(
+  "/todo",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const todo = new Todo(req.body);
+    todo.save((err) => {
+      if (err)
+        res
+          .status(500)
+          .json({ message: { msgBody: "Error has occured", msgError: true } });
+      else {
+        req.user.todos.push(todo);
+        req.user.save((err) => {
+          if (err)
+            res.status(500).json({
+              message: { msgBody: "Error has occured", msgError: true },
+            });
+          else
+            res.status(200).json({
+              message: {
+                msgBody: "Succesfully created todo",
+                msgError: false,
+              },
+            });
+        });
+      }
+    });
+  }
+);
+userRouter.get(
+  "/todos",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    User.findById({ _id: req.user._id })
+      .populate("todos")
+      .exec((err, document) => {
+        if (err)
+          res.status(500).json({
+            message: { msgBody: "Error has occured", msgError: true },
+          });
+        else {
+          res.status(200).json({ todos: document.todos, authenticated: true });
+        }
+      });
+  }
+);
+
+userRouter.get(
+  "/admin",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    if (req.user.role === "admin") {
+      res
+        .status(200)
+        .json({ message: { msgBody: "You are an admin", msgError: false } });
+    } else
+      res.status(403).json({
+        message: { msgBody: "You are not an admin, go away", msgError: true },
+      });
+  }
+);
+
+userRouter.get(
+  "/authenticated",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { username, role } = req.user;
+    res.status(200).json({ isAuthenticated: true, user: { username, role } });
+  }
+);
+
 module.exports = userRouter;
